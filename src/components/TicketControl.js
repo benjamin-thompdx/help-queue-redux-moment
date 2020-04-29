9697acfc8eeddf949876be45f89e6fd359de59c6
Benjamin Thom <jaminthom@gmail.com>
2020-04-29 07:59:03 -0700

    create an actions folder within __tests__ and write test for DELETE_TICKET action

--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -3,7 +3,7 @@ import NewTicketForm from './NewTicketForm';
 import TicketList from './TicketList';
 import TicketDetail from './TicketDetail';
 import EditTicketForm from './EditTicketForm';
-import {connect } from 'react-redux';
+import { connect } from 'react-redux';
 import { act } from 'react-dom/test-utils';
 import PropTypes from 'prop-types';
 
