4866cd5f0264711db02fad947e948f8eb9512f61
Benjamin Thom <jaminthom@gmail.com>
2020-04-29 18:28:34 -0700

    write an UPDATE_TIME test within src/__tests__/reducers/ticket-list-reducer.test.js, successfully fail it, write a case within src/reducers/ticket-list-reducer.js for UPDATE_TIME, and successfully pass test

--- src/__tests__/reducers/ticket-list-reducer.test.js
+++ src/__tests__/reducers/ticket-list-reducer.test.js
@@ -19,7 +19,8 @@ describe('ticketListReducer', () => {
     names: 'names solo',
     location: 'location solo',
     issue: 'issue solo',
-    id:1
+    timeOpen: 0,
+    id: 1
   };
 
   test('Should return default state if no action type is recognized', ()=> {
@@ -56,6 +57,25 @@ describe('ticketListReducer', () => {
       location: 'location 2',
       issue: 'issue 2',
       id: 2 }
+    });
+  });
+
+  test('Should add a formatted wait time to ticket entry', () => {
+    const { names, location, issue, timeOpen, id } = ticketData;
+    action = {
+      type: c.UPDATE_TIME,
+      formattedWaitTime: '4 minutes',
+      id: id
+    };
+    expect(ticketListReducer({ [id]: ticketData }, action)).toEqual({
+      [id]: {
+        names: names,
+        location: location,
+        issue: issue,
+        timeOpen: timeOpen,
+        id: id,
+        formattedWaitTime: '4 minutes'
+      }
     });
   });
 
