a40c0239973ebd342312493eba9c089a9826a430
Benjamin Thom <jaminthom@gmail.com>
2020-04-29 17:32:24 -0700

    install moment.js and update src/actions/ActionTypes.js sytax

--- src/actions/ActionTypes.js
+++ src/actions/ActionTypes.js
@@ -1,5 +1,3 @@
-import * as c from './../actions/ActionTypes';
-
-export const ADD_TICKET = c.ADD_TICKET;
-export const DELETE_TICKET = c.DELETE_TICKET;
-export const TOGGLE_FORM = c.TOGGLE_FORM;
+export const ADD_TICKET = 'ADD_TICKET';
+export const DELETE_TICKET = 'DELETE_TICKET';
+export const TOGGLE_FORM = 'TOGGLE_FORM';
